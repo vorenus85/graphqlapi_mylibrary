a37669e897f72c9658cb1e71ddfea7f1e595ab75
Janos Perge <janos.perge@optimonk.com>
2023-04-07 13:14:23 +0200

    add books.json data

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql, ApolloError} = require('apollo-server');
+const books = require('./data/books.json')
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -8,8 +9,10 @@ const typeDefs = gql`
 
   # This "Book" type defines the queryable fields: 'title' and 'author'.
   type Book {
-    title: String
-    author: String
+    title: String!
+    author: String!
+    tags: [String],
+    isRead: Boolean!
   }
 
   # The "Query" type is special: it lists all of the available queries that
@@ -19,17 +22,6 @@ const typeDefs = gql`
     books: [Book]
   }
 `;
-
-const books = [
-    {
-      title: 'The Great Gatsby',
-      author: 'F. Scott Fitzgerald',
-    },
-    {
-      title: 'Wuthering Heights',
-      author: 'Emily Brontë',
-    },
-  ];
 
   // Resolvers define the technique for fetching the types defined in the
 // schema. This resolver retrieves books from the "books" array above.
