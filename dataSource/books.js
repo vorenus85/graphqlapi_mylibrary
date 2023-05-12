191dd555e51658b29cd5581125a19e118b244200
Janos Perge <janos.perge@optimonk.com>
2023-05-12 22:32:35 +0200

    separate db tables to fix avoid effects

--- dataSource/books.js
+++ dataSource/books.js
@@ -6,7 +6,7 @@ const lodashId = require('lodash-id');
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 
-const adapter = new FileSync('./db.json');
+const adapter = new FileSync('./data/books.json');
 const db = low(adapter);
 db._.mixin(lodashId);
 
